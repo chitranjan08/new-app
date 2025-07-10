96878d413ddd74f1a317972e8008caafe825061b
Chitranjan Kumar <chitranjank80@gmail.com>
2025-07-11 01:09:49 +0530

    3

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
-  res.send('Hello Chitranjan kumar2');
+  res.send('Hello Chitranjan kumar3');
 });
 // Start server
 app.listen(3000, '0.0.0.0', () => {
