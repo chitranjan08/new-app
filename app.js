8401aa9c30253868bcdf689fb14525567749e330
Chitranjan Kumar <chitranjank80@gmail.com>
2025-07-10 20:39:16 +0530

    docker compose

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
-  res.send('Hello Chitranjan kumar yadav');
+  res.send('Hello Chitranjan kumar1');
 });
 // Start server
 app.listen(3000, '0.0.0.0', () => {
